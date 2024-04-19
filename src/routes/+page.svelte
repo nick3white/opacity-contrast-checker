fd527a79c72e5408888017519d2640c2e7ad176d
N W <nick33white@gmail.com>
2024-04-19 08:38:27 -0500

    add more explainer text; add photo credit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import { browser } from '$app/environment';
 	import ColorThief from 'colorthief';
 	import 'carbon-components-svelte/css/g90.css';
-	import { Dropdown, FileUploader, Slider } from 'carbon-components-svelte';
+	import { CopyButton, Dropdown, FileUploader, Slider } from 'carbon-components-svelte';
 	import { calculateContrastRatio, objectify } from '$lib';
 	import { hexToRGB, rgbToHex } from '$lib/colorUtils';
 	let colorThief,
@@ -27,9 +27,12 @@
 		useImageColors && palette[schema[1].id] ? palette[schema[1].id] : overlayColorPicker;
 	$: fgColor = useImageColors && palette[schema[2].id] ? palette[schema[2].id] : fgColorPicker;
 
+	// TODO: refactor schema to allow colorpicker input after image upload
+
 	onMount(async () => {
 		colorThief = new ColorThief();
 	});
+
 	async function handleFileUpload(event) {
 		const file = event.detail.length > 0 ? event.detail[0] : false;
 		// const file = event.detail[0];
@@ -85,6 +88,11 @@
 	let open = false;
 	$: resultRGB = hexToRGB(computedColor);
 	$: contrastRatio = calculateContrastRatio(hexToRGB(computedColor), hexToRGB(fgColor));
+	$: fixUsersBadContrast =
+		contrastRatio < 4.5
+			? `background: #393939; color: rgba(233,233,255,0.9);`
+			: `background: rgba(0,0,0,0); color: ${fgColor};`;
+
 	function cssizer(color) {
 		if (typeof color === 'string') {
 			color = hexToRGB(color);
@@ -95,18 +103,19 @@
 		}
 		return `${color[triad[0]]}, ${color[triad[1]]}, ${color[triad[2]]}`;
 	}
-	// // if (useImageColors) {
-	// 	$: bgColor = palette[schema[0].id];
-	// 	$: overlayColor = palette[schema[1].id];
-	// 	$: fgColor = palette[schema[2].id];
-	// 	$: console.log('fgcolor', fgColor);
-	// }
 	$: console.log('palette', palette);
 
 	$: console.log('background color', bgColor);
 	$: console.log('scjema', schema);
 </script>
 
+<p class:hide={imageUrl !== 'tunnel.jpg'} style={fixUsersBadContrast} class="photo-credit">
+	photo by <a
+		href="https://www.flickr.com/photos/megane_wakui"
+		style={fixUsersBadContrast}
+		target="_blank">Masashi Wakui</a
+	>
+</p>
 <div
 	class="wrapper"
 	id="wrapper"
@@ -124,7 +133,7 @@
 				on:change={handleFileUpload}
 			/>
 			{#if useImageColors}
-				{#each schema as color}
+				{#each schema as color, i}
 					<Dropdown
 						style="--active-color: {palette[color.id]} !important;"
 						titleText={`${color.loc} Color`}
@@ -137,54 +146,97 @@
 				{/each}
 			{:else}
 				<div class="input-wrapper">
-					<label for="bgPicker">Background Color:</label>
+					<label for="bgPicker">Background Color: </label>
 					<input type="color" class="color-picker" id="bgPicker" bind:value={bgColorPicker} />
 				</div>
 				<div class="input-wrapper">
-					<label for="olPicker">Overlay Color:</label>
+					<label for="olPicker">Overlay Color: </label>
 					<input type="color" class="color-picker" id="olPicker" bind:value={overlayColorPicker} />
 				</div>
 				<div class="input-wrapper">
-					<label for="fgPicker">Foreground Color:</label>
+					<label for="fgPicker">Foreground Color: </label>
 					<input type="color" class="color-picker" id="fgPicker" bind:value={fgColorPicker} />
 				</div>
 			{/if}
 			<div class="more-info">
-				<em class="teaser" class:open on:click={() => (open = !open)}>What is this i don't even</em>
+				<button class="teaser" class:open on:click={() => (open = !open)}
+					>What is this i don't even</button
+				>
 				<div class="more-info-content">
 					<p>
+						This tool calculates the contrast ratio of text on background when using a
+						semi-transparent overlay. Provide a background color, an overlay color, and a
+						foreground/text color, and the tool will calculate the contrast ratio, seen in the
+						bottom right.
+					</p>
+					<p>
+						The <a
+							href="https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html"
+							target="_blank">WCAG guidelines</a
+						>
+						use the following thresholds:
+						<span class="lightgreen">greater than 4.5:1 is "AA" status;</span>
+						<span class="yellow">3.0-4.5 is "A";</span>
+						<span class="pink"> less than 3 is <em>fail.</em></span>
+					</p>
+					<p>
+						If you choose an image of your own, the tool will generate a 5-color palette from the
+						dominant colors of the image, using <a
+							href="https://lokeshdhakar.com/projects/color-thief/"
+							target="_blank">Color Thief</a
+						>. (This tool doesn't provide css for the full palette, the Color Thief website provides
+						that functionality, and it's a bit out of scope for this already-bloated tool.)
+					</p>
+					<p>
+						Why? Because it's a deceptively difficult task. There are plenty of tools online that
+						will check contrast ratio, they don't often make it easy to use opacity-based blend and
+						then to tweak the colors to get a decent ratio. The idea of this tool is that it allows
+						you to tweak the knobs a bit better. Additionally, the image palette generation part
+						isn't super common.
+					</p>
+					<p>
+						<em>
+							Please note that this tool does not upload anything to a server. There's no storage,
+							no memory, nothing like that. The only data collected by the tool is traffic, and I
+							don't sell your data.
+						</em>
+					</p>
+					<p>
 						Once upon a time, I made a background with an overlay. The contrast was great! But then
-						my "stakeholder" felt the background was too dark. Is that even possible?
+						my "stakeholder" felt the background was too dark. <em>Too</em> dark. Is that even possible?
 					</p>
 					<p>
 						She gave me a target color, and asked me to blend the background color with the overlay
 						to reach that target.
 					</p>
 					<p>
-						I don't know if you know anything about color theory, but that's some weird math. So I
-						made a thing. This way, I never have to do math, and I can help you avoid it, too.
+						I don't know if you know anything about color theory, but the math involved in that
+						request is...from another planet. So I made a thing. This way, I never have to do that
+						math, and, hopefully, with this lil tool, you can avoid it, too.
 					</p>
 				</div>
 			</div>
 		</div>
 		<div class="lilwrapper" id="color-display">
 			<div class="intro">
-				<p>{bgColor} with {overlayColor} at {(opacity * 100).toFixed(0)}%</p>
+				<p>
+					text: {fgColor}
+				</p>
+				<p>on background: {bgColor}</p>
+
+				<p>
+					with overlay: {overlayColor} at {(opacity * 100).toFixed(0)}%
+				</p>
 				<p>looks kinda like:</p>
 			</div>
 			<div class="rgb" style="background-color: rgb({cssizer(resultRGB)}); text-align: center;">
-				<p style="display: inline-block; padding: 4px; margin: 4px; color: {fgColor}">
+				<p style=" color: {fgColor}">
 					rgb: {cssizer(resultRGB)}
+					<CopyButton text={cssizer(resultRGB)} feedback="Copied!">ok</CopyButton>
 				</p>
 			</div>
 			<div class="contrast">
-				<p
-					style="color: {contrastRatio < 3
-						? 'pink'
-						: contrastRatio > 4.5
-							? 'lightgreen'
-							: 'yellow'}"
-				>
+				<p class={contrastRatio < 3 ? 'pink' : contrastRatio > 4.5 ? 'lightgreen' : 'yellow'}>
 					contrast ratio: {contrastRatio}
 				</p>
 			</div>
@@ -209,6 +261,50 @@
 </div>
 
 <style>
+	.dropdown-wrapper {
+		width: 100%;
+		display: flex;
+		gap: 11px;
+		&:first-child {
+			flex: 1;
+		}
+		&:last-child {
+			flex-basis: 64px;
+		}
+	}
+	.lightgreen {
+		color: lightgreen !important;
+	}
+	.yellow {
+		color: yellow !important;
+	}
+	.pink {
+		color: pink !important;
+	}
+	a {
+	}
+	.rgb p {
+		display: flex;
+		justify-content: start;
+		align-items: center;
+		gap: 11px;
+		padding: 4px;
+		margin-block: 4px;
+		&:last-child {
+			justify-content: end;
+		}
+	}
+	.photo-credit {
+		z-index: 4;
+		position: absolute;
+		top: 0;
+		right: 0;
+		padding: 3px;
+		margin: 3px;
+	}
+	.photo-credit.hide {
+		display: none;
+	}
 	.teaser {
 		display: block;
 		background: rgba(0, 0, 0, 0.33);
@@ -247,6 +343,14 @@
 			height: 100%;
 		}
 	}
+	:global(.bx--copy-btn.bx--copy) {
+		background: rgba(0, 0, 0, 0.33);
+		border: 1px solid rgba(0, 0, 0, 0.77);
+		&:hover {
+			background: rgba(0, 0, 0, 0.77);
+			border: 1px solid rgba(0, 0, 0, 1);
+		}
+	}
 	:global(.bx--dropdown) {
 		background-color: var(--active-color) !important;
 	}
@@ -278,7 +382,6 @@
 	.wrapper {
 		/* overflow: hidden; */
 		position: relative;
-
 		background-size: cover;
 		display: flex;
 		flex-direction: column;
@@ -310,8 +413,8 @@
 	@media screen and (min-width: 876px) {
 		.wrapper {
 			/* overflow: auto; */
-			height: 100vh;
-			width: 100vw;
+			min-height: 100vh;
+			min-width: 100vw;
 		}
 		.infobox {
 			justify-content: space-between;
@@ -335,6 +438,7 @@
 	.underlay {
 		clip-path: polygon(0 0, 100% 0, 100% 33%, 33% 100%, 0 100%);
 		object-fit: cover;
+
 		height: 100%;
 		width: 100%;
 		z-index: 1;
@@ -376,7 +480,10 @@
 	#color-display p {
 		font-family: 'Monaspace Xenon';
 		color: #ccc;
-		text-align: right;
+		&:last-child {
+			margin-top: 11px;
+			text-align: right;
+		}
 	}
 	.slider-wrapper {
 		z-index: 3;
